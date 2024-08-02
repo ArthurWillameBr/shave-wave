8b19ba12c05251966a743c24f62a67b3e1312b27
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-02 20:16:26 -0300

    chore: add shadcn/ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Helo, World</h1>
+    <div className="flex min-h-screen items-center justify-center bg-neutral-900">
+      <Button variant="outline" className="shadow-2xl">Test</Button>
     </div>
   )
 }

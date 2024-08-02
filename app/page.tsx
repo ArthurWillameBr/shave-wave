4b3cd32022333c5d794203eb7d8dd3a6f11ab9e6
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-02 20:22:26 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "./_components/ui/button"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-neutral-900">
+    <div className="flex min-h-screen items-center justify-center bg-primary">
       <Button variant="outline" className="shadow-2xl">Test</Button>
     </div>
   )

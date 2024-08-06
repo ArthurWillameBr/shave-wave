2aff09741292eb51aaf6a9806e65f2b7887d26da
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-06 14:48:20 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,57 @@
+import { Search } from "lucide-react"
 import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className="p-5">
+        <h1 className="text-xl font-bold">Olá, Arthur</h1>
+        <p>Terça-feira, 06 de maio</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <Search />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px]">
+          <Image
+            alt="banner"
+            src="/banner01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmar</Badge>
+              <h3 className="font-bold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="size-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia Elite</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">20:30</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </>
   )
 }

780d0f10e6420c44b6b9bfc56f8b9a29f386d646
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-08 11:28:17 -0300

    feat: add sign-in with Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "./_components/ui/sonner"
 import Footer from "./_components/footer"
+import AuthProvider from "./_providers/auth"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="pt" className="dark">
       <body className={inter.className}>
-        {children}
-        <Footer />
-        <Toaster richColors />
+        <AuthProvider>
+          {children}
+          <Footer />
+          <Toaster richColors />
+        </AuthProvider>
       </body>
     </html>
   )
